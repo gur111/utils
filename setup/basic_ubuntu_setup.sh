428b9d041242b5b1fa9e6a1fac08d5e6c0303616
gur111 <gur111@users.noreply.github.com>
2020-04-21 14:41:37 -0700

    Add vscode and clion

--- setup/basic_ubuntu_setup.sh
+++ setup/basic_ubuntu_setup.sh
@@ -4,6 +4,8 @@ set -e
 
 sudo apt update && sudo apt upgrade
 sudo apt install curl wget git zsh
+sudo snap install code --classic
+sudo snap install clion --classic
 wget https://www.scootersoftware.com/bcompare-4.3.4.24657_amd64.deb
 sudo dpkg -i bcompare-4.3.4.24657_amd64.deb
 rm bcompare-4.3.4.24657_amd64.deb
