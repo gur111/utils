a62b8b2afe5e48f5c240c620216c95303ac0f915
Gur Telem <gur111telem@gmail.com>
2020-05-08 00:58:53 +0300

    Add typora to basic setup

--- setup/basic_ubuntu_setup.sh
+++ setup/basic_ubuntu_setup.sh
@@ -5,6 +5,13 @@ set -e
 sudo apt update && sudo apt upgrade -y
 sudo apt install curl wget git parcellite -y
 
+# Add Typora repo
+wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+sudo add-apt-repository 'deb https://typora.io/linux ./'
+sudo apt update
+sudo apt install typora -y
+
+
 # Install Google Drive Mounting thingy.
 # Auto mounting is confed in the i3 config
 sudo add-apt-repository ppa:alessandro-strada/ppa
