fc30302cea5bac15fead986756e064d0b1d96e2f
Gur Telem <gur111telem@gmail.com>
2020-06-13 16:57:52 +0300

    Different keyboard layout per window

--- i3-configs/setup.sh
+++ i3-configs/setup.sh
@@ -1,6 +1,6 @@
 
 
-sudo apt install i3
+sudo apt install i3 kbdd
 ln -s ~/Documents/git/utils/i3-configs/config ~/.config/i3/config
 
 sed -i -- 's/%{%k%F{$CURRENT_BG}%}$SEGMENT_SEPARATOR/%{%k%F{$CURRENT_BG}%}\\n$SEGMENT_SEPARATOR/g' ~/.oh-my-zsh/themes/agnoster.zsh-theme
